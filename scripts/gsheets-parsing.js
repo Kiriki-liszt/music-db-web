45fde4b724959272ba346c6ea52af0a5d0207323
Kiriki-liszt <kirikiaris@gmail.com>
2022-10-30 18:17:13 +0900

    try

--- scripts/gsheets-parsing.js
+++ scripts/gsheets-parsing.js
@@ -25,7 +25,7 @@ request.onload = function() {
         console.log(rows);
     }
     musicbook = JSON.parse(musicbookText[1]).table.rows.map(({c}) => c.map(e => e ? (e.v || "") : ""));
-    musicbook.removeChild(musicbook[0]);
+    delete musicbook.가수;
     const ASIS = musicbook;
 
     unOrdered = ASIS;
